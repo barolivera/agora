36195f1b03e2fedc3387f6a831815ecb576bc6c0
Barb <brb.olivera@gmail.com>
2026-03-13 21:28:49 -0300

    feat: initial commit

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,9 +4,3 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-
-export function deslugify(slug: string): string {
-  return slug
-    .replace(/-/g, ' ')
-    .replace(/\b\w/g, (c) => c.toUpperCase())
-}
